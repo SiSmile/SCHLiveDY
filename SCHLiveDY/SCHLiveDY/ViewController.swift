c95b334a30103611945e58f6000bc6668124f93f
SiSmile <35027447+SiSmile@users.noreply.github.com>
2018-01-25 16:24:10 +0800

    初始化项目

---
+++ SCHLiveDY/SCHLiveDY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SCHLiveDY
+//
+//  Created by Mike on 2018/1/25.
+//  Copyright © 2018年 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
